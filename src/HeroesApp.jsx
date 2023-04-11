4f93c1a81010d84597e874f087463f5d69c9c564
Edward Arredondo Maleno <edwardmanuelarredondo@outlook.es>
2023-04-11 16:57:23 -0400

    segundo commit

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,16 @@
+import { AuthProbider } from './auth';
+import { AppRouter } from './router/AppRouter';
+
+
+export const HeroesApp = () => {
+  return (
+
+    <AuthProbider>
+      
+      <AppRouter />
+
+    </AuthProbider>
+
+
+  )
+}
---
+++ src/auth/context/index.js
@@ -0,0 +1,3 @@
+export * from './AuthContext'
+export * from './AuthProbider'
+export * from './AuthReducer'
